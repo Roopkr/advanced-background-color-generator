88a4516c919df00de0bd543f5653ab0ac14f6fc2
Roopkr <61685476+Roopkr@users.noreply.github.com>
2022-02-04 00:32:59 +0530

    Code commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let body = document.querySelector('body');
+let input1 = document.querySelector('.color1');
+let input2 = document.querySelector('.color2');
+let select = document.querySelector('#direction');
+let css = document.querySelector('h3');
+let changeGradient = function() {
+    if (select.value.length == 0) {
+        body.style.background = "linear-gradient(" + input1.value + "," + input2.value + ")";
+    }
+    body.style.background = "linear-gradient(" + select.value + "," + input1.value + "," + input2.value + ")";
+    css.textContent = body.style.background;
+}
+
+input1.addEventListener('input', changeGradient);
+input2.addEventListener('input', changeGradient);
+select.addEventListener('input', changeGradient);
